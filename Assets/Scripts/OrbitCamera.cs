df8d8db5ae141f21dad3e6a0feccc2ac75ef4836
Daniel Pascual López <uo269728@uniovi.es>
2024-07-11 18:18:25 +0200

    Mover camara

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -12,16 +12,34 @@ public class OrbitCamera : MonoBehaviour {
 
     [SerializeField, Range(0f, 1f)]
 	float focusCentering = 0.5f;
+
+	[SerializeField, Range(1f, 360f)]
+	float rotationSpeed = 90f;
     Vector3 focusPoint;
+
+	Vector2 orbitAngles = new Vector2(45f, 0f);
 
 	void Awake () {
 		focusPoint = focus.position;
 	}
-
-    void LateUpdate () {
+	void LateUpdate () {
 		UpdateFocusPoint();
-		Vector3 lookDirection = transform.forward;
-		transform.localPosition = focusPoint - lookDirection * distance;
+		ManualRotation();
+		Quaternion lookRotation = Quaternion.Euler(orbitAngles);
+		Vector3 lookDirection = lookRotation * Vector3.forward;
+		Vector3 lookPosition = focusPoint - lookDirection * distance;
+		transform.SetPositionAndRotation(lookPosition, lookRotation);
+	}
+
+	void ManualRotation () {
+		Vector2 input = new Vector2(
+			Input.GetAxis("Vertical Camera"),
+			Input.GetAxis("Horizontal Camera")
+		);
+		const float e = 0.001f;
+		if (input.x < -e || input.x > e || input.y < -e || input.y > e) {
+			orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+		}
 	}
     void UpdateFocusPoint () {
 		Vector3 targetPoint = focus.position;

e45b6faf1506ca492eecb089b6bf0fae5d9c7cbd
Daniel Pascual López <uo269728@uniovi.es>
2024-07-18 23:38:43 +0200

    mas axis

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -32,7 +32,7 @@ public class MovingSphere : MonoBehaviour
 	[SerializeField]
 	Transform playerInputSpace = default;
 
-	Vector3 upAxis;
+	Vector3 upAxis, rightAxis, forwardAxis;
 
 	Vector3 velocity, desiredVelocity;
 
@@ -73,19 +73,19 @@ public class MovingSphere : MonoBehaviour
 		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
 		if (playerInputSpace) {
-			Vector3 forward = playerInputSpace.forward;
-			forward.y = 0f;
-			forward.Normalize();
-			Vector3 right = playerInputSpace.right;
-			right.y = 0f;
-			right.Normalize();
-			desiredVelocity =
-				(forward * playerInput.y + right * playerInput.x) * maxSpeed;
+			rightAxis = ProjectDirectionOnPlane(playerInputSpace.right, upAxis);
+			forwardAxis =
+				ProjectDirectionOnPlane(playerInputSpace.forward, upAxis);
 		}
 		else {
-			desiredVelocity =
-				new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+			rightAxis = ProjectDirectionOnPlane(Vector3.right, upAxis);
+			forwardAxis = ProjectDirectionOnPlane(Vector3.forward, upAxis);
 		}
+		desiredVelocity =
+			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		
+		
+		desiredJump |= Input.GetButtonDown("Jump");
 
 		
 	}
@@ -235,16 +235,15 @@ public class MovingSphere : MonoBehaviour
 		return true;
 	}
 
-	Vector3 ProjectOnContactPlane(Vector3 vector)
-	{
-		return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+	Vector3 ProjectDirectionOnPlane (Vector3 direction, Vector3 normal) {
+		return (direction - normal * Vector3.Dot(direction, normal)).normalized;
 	}
 
 
 	void AdjustVelocity()
 	{
-		Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
-		Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+		Vector3 xAxis = ProjectDirectionOnPlane(rightAxis, contactNormal);
+		Vector3 zAxis = ProjectDirectionOnPlane(forwardAxis, contactNormal);
 		float currentX = Vector3.Dot(velocity, xAxis);
 		float currentZ = Vector3.Dot(velocity, zAxis);
 		float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;

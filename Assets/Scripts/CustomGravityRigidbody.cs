65b11a06fd91192a2dd214966f9ffa3b1d540f26
Daniel Pascual López <uo269728@uniovi.es>
2024-07-23 21:19:08 +0200

    Planeta simple

--- Assets/Scripts/CustomGravityRigidbody.cs
+++ Assets/Scripts/CustomGravityRigidbody.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class CustomGravityRigidbody : MonoBehaviour {
+public class CustomGravityRigidbody : MonoBehaviour
+{
 
-	Rigidbody body;
+    [SerializeField]
+    bool floatToSleep = false;
 
-	void Awake () {
-		body = GetComponent<Rigidbody>();
-		body.useGravity = false;
-	}
+    float floatDelay;
 
-    void FixedUpdate () {
-		body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
-	}
+    Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.useGravity = false;
+    }
+    void FixedUpdate()
+    {
+
+        if (floatToSleep)
+        {
+
+
+            if (body.IsSleeping())
+            {
+                floatDelay = 0f;
+                return;
+            }
+
+            if (body.velocity.sqrMagnitude < 0.0001f)
+            {
+                floatDelay += Time.deltaTime;
+                if (floatDelay >= 1f)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                floatDelay = 0f;
+            }
+        }
+        body.AddForce(
+            CustomGravity.GetGravity(body.position), ForceMode.Acceleration
+        );
+    }
 }

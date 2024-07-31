5f865a4df50e757346eb12fe83506a284c2cd910
Daniel Pascual López <uo269728@uniovi.es>
2024-07-31 20:54:38 +0200

    base para diferentes tipos de gravedad

--- Assets/Scripts/GravitySource.cs
+++ Assets/Scripts/GravitySource.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class GravitySource : MonoBehaviour {
 
-	public Vector3 GetGravity (Vector3 position) {
+	public virtual Vector3 GetGravity (Vector3 position) {
 		return Physics.gravity;
 	}
+	void OnEnable () {
+		CustomGravity.Register(this);
+	}
+
+	void OnDisable () {
+		CustomGravity.Unregister(this);
+	}
+	
 }
